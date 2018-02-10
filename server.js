87c18f46e543492d0e73fefe15659a6eed17b6d1
Ivan Moreno <ivan.moreno1@g.austincc.edu>
2018-02-09 23:41:16 -0600

    added html in public folder

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
+// DEPENDENCIES
 // =============================================================
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
 
-var apiRoutes = require('./app/routing/apiRoutes.js')
-var htmlRoutes = require('./app/routing/htmlRoutes.js')
-
 // Sets up the Express App
-// =============================================================
+
 var app = express()
+// Port
 var PORT = process.env.PORT || 3000
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+var apiRoutes = require('./app/routing/apiRoutes.js')
+var htmlRoutes = require('./app/routing/htmlRoutes.js')
+
 
 app.use(apiRoutes)
 app.use(htmlRoutes)
